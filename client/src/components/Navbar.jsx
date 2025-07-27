782ec3405d6b490607beef45f12bdaed268836bf
DESKTOP-C83VT1A\Ahmed Nasir <ahmed.nasir@tabsusa.com>
2025-07-27 16:29:49 +0500

    n

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,16 @@
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
-import logo from '../assets/logo.png'; // Update this path if necessary
+import logo from '../assets/logo.png'; // make sure the path is correct
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-top">
-        <div className="navbar-brand">
-          <img src={logo} alt="Logo" className="navbar-logo" />
-          <NavLink to="/" className="navbar-title">Ahmed Nasir</NavLink>
-        </div>
-        <input type="checkbox" id="toggle" className="nav-toggle" />
-        <label htmlFor="toggle" className="hamburger">&#9776;</label>
+      <div className="navbar-brand">
+        <img src={logo} alt="Logo" className="navbar-logo" />
+        <NavLink to="/" className="navbar-title">Ahmed Nasir</NavLink>
       </div>
-
+      <input type="checkbox" id="toggle" className="nav-toggle" />
+      <label htmlFor="toggle" className="hamburger">&#9776;</label>
       <div className="navbar-links">
         <NavLink to="/" end className="nav-link">Home</NavLink>
         <NavLink to="/projects" className="nav-link">Projects</NavLink>
